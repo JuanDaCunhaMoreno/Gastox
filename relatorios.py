b3d73a968a451b71596e6a568b0c8886e69e53af
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 11:58:19 -0300

    Update relatorios.py

--- relatorios.py
+++ relatorios.py
@@ -7,3 +7,22 @@ def tot_categoria(lista_despesas):
             totais[categoria] = 0
         totais[categoria] += despesa.valor
     return totais
+
+
+def estat_gerais(lista_despesas):
+    if not lista_despesas:
+        return None #Sem despesa
+    
+    total = sum(d.valor for d in lista_despesas)
+    quantidade = len(lista_despesas)
+    media = total / quantidade
+    maior = max(lista_despesas, key=lambda d: d.valor)
+    menor = min(lista_despesas, key=lambda d: d.valor)
+
+    return {
+        "total": total,
+        "quantidade": quantidade,
+        "media": media,
+        "maior": maior,
+        "menor": menor
+    }
