2e4d1821623eec81e04b8f65824a7e77e5a5d96b
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 11:00:08 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from models import Despesa
+from database import adicionar_despesa, listar_despesas
+from time import sleep
+
+def exibir_menu():
+    print("\n1 - Cadastrar gasto")
+    print("4 - Listar gastos")
+    print("0 - Sair")
+
+def executar():
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == '1':
+            id = input("ID: ")
+            valor = float(input("Valor: "))
+            descricao = input("Descrição: ")
+            data = input("Data: ")
+            categoria = input("Categoria: ")
+            nova_despesa = Despesa(id, valor, descricao, data, categoria)
+            adicionar_despesa(nova_despesa)
+            print("Gasto adicionado!")
+
+        elif opcao == '4':
+            for despesa in listar_despesas():
+                print(despesa)
+        elif opcao == '0':
+            print("Saindo...")
+            sleep(0.5)
+            break
+
+if __name__ == "__main__":
+    executar()
+    
