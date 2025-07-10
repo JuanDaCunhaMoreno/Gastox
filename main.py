8a14077d7f3232b2ea363d60550086c0f92f625e
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 11:17:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from models import Despesa
-from database import adicionar_despesa, listar_despesas
+from database import adicionar_despesa, listar_despesas, remover_despesas
 from time import sleep
 
 def exibir_menu():
     print("\n1 - Cadastrar gasto")
+    print("2 - Editar gasto")
+    print("3 - Remover gasto")
     print("4 - Listar gastos")
+    print("5 - Exibir total por categoria")
+    print("6 - Estatísticas gerais")
+    print("7 - Exportar dados")
     print("0 - Sair")
 
 def executar():
@@ -21,6 +26,15 @@ def executar():
             adicionar_despesa(nova_despesa)
             print("Gasto adicionado!")
 
+        elif opcao == '2':
+
+        elif opcao == '3':
+            removerID = input("Digite o ID para ser removido: ")
+            if remover_despesas(removerID):
+                print("Despesa removida com sucesso!")
+            else:
+                print("Despesa não encontrada!")
+
         elif opcao == '4':
             for despesa in listar_despesas():
                 print(despesa)
@@ -31,4 +45,3 @@ def executar():
 
 if __name__ == "__main__":
     executar()
-    
