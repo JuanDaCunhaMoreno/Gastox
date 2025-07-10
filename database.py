ae0fe8031254e0918cb040387f942a4e14ea6996
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 11:00:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 despesas = []
-def adicionar_despesas(despesas):
+def adicionar_despesa(despesas):
     despesas.append(despesas)
 
 def listar_despesas():
     return despesas
 
 def editar_despesas():
-    
