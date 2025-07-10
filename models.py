32785a6e970106ea5287fa4116084a7738c1634d
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 10:48:09 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+class Despesa:
+    def __init__(self, id, valor, descricao, data, categoria):
+        self.id = id
+        self.valor = valor
+        self.descricao = descricao
+        self.data = data
+        self.categoria = categoria
+    def __str__(self):
+        return f"{self.id} | {self.valor:.2f} | {self.descricao} | {self.data} | {self.categoria}" 
