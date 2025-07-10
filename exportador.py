914a30c95ea5ba7506b5f76e8193c64ae9a6e0c6
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-10 16:58:30 -0300

    Create exportador.py

---
+++ exportador.py
@@ -0,0 +1,20 @@
+#Exportador
+import csv
+
+def exportar_despesas(lista_despesas, nome_arquivo="Despesas_exportadas"):
+    with open(nome_arquivo, mode="w", newline="", encoding="utf-8") as arquivo_csv:
+        writer = csv.writer(arquivo_csv)
+
+#Cabeçalho CSV
+        writer.writerow(["ID", "Valor", "Descrição", "Data", "Categoria"])
+#Linhas com os dados
+        for despesa in lista_despesas:
+            writer.writerow([
+                despesa.id,
+                f"{despesa.valor:.2f}",
+                despesa.descricao,
+                despesa.data,
+                despesa.categoria
+            ])
+
+    return nome_arquivo #Retorna o nome do arquivo gerado
